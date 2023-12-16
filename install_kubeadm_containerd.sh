22d24be02f6a636b195e896ee772af659dfddbaa
son.lethanh10 <son.lethanh10@mobifone.vn>
2023-12-16 16:36:54 +0700

    update doc

--- install_kubeadm_containerd.sh
+++ install_kubeadm_containerd.sh
@@ -99,7 +99,8 @@ ARCH="amd64"
 curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | sudo tar -C $DOWNLOAD_DIR -xz
 
 # Install kubeadm, kubelet, kubectl (Version 1.28):
-RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+# RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+RELEASE="v1.28.2"
 ARCH="amd64"
 cd $DOWNLOAD_DIR
 sudo curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
