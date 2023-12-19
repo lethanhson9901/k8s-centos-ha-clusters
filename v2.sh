73ea4637d41ec17ed7d147930911b3f46dc65f82
son.lethanh10 <son.lethanh10@mobifone.vn>
2023-12-19 12:00:35 +0700

    update README.EADME.md

--- v2.sh
+++
@@ -1,5 +0,0 @@
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/custom-resources.yaml -O
-kubectl create -f custom-resources.yaml
-
-kubectl get po -n kube-system
